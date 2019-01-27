1e415024986c66e21305f3f4c92298daec4534f2
damien tohin <damientohin@gmail.com>
2019-01-27 10:29:44 +0100

    I added the basic command line parser

---
+++ ResXTranslator/Parsers/CLIParser.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResXTranslator.Parsers
+{
+    public class CLIParser
+    {
+        /// <summary>
+        /// The path to the resx file you wish to translate.
+        /// </summary>
+        [Option('f', "file", Required = true, HelpText = "The path to the resx file you wish to translate.")]
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Languages the resource file will be translated to
+        /// </summary>
+        [Option('t', "translations", Separator = ',', Required = true, HelpText = "Languages to which you want your resource to be translated to.")]
+        public IEnumerable<string> TranslationLanguages { get; set; }
+
+        /// <summary>
+        /// The language of your current resource file.
+        /// </summary>
+        [Option('l', "language", HelpText = "The language of your current resource file.")]
+        public string ResourceLanguage { get; set; }
+    }
+}
