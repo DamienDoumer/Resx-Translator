2c5452b11235b789f2426be9d68a02c22604ed4b
damien tohin <damientohin@gmail.com>
2019-01-28 01:08:08 +0100

    Added program lunch function

--- ResXTranslator/Program.cs
+++ ResXTranslator/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Translation.V2;
+using ResXTranslator.Parsers;
+using ResXTranslator.ResxHandler;
+using ResXTranslator.TranslationServices;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ResXTranslator
 {
@@ -8,12 +14,28 @@ namespace ResXTranslator
     {
         static void Main(string[] args)
         {
-            var credential = GoogleCredential.FromFile("APIKey.json");
-            TranslationClient client = TranslationClient.Create(credential);
-            TranslationResult result = client.TranslateText("It is raining.", LanguageCodes.French);
-            Console.WriteLine($"Result: {result.TranslatedText}; detected language {result.DetectedSourceLanguage}");
-            Console.ReadKey();
+            CLIParser.Parse(new List<string>(args), Run);
+        }
 
+        static async void Run(CLIOptions options)
+        {
+            IResxHandler resxHandler = new SimpleResxHandler();
+            ITranslator translator = new GoogleTranslation();
+
+            var resources = resxHandler.Read(options.FilePath);
+            var words = resources.Select(kv => kv.Value);
+            foreach (var lge in options.TranslationLanguages)
+            {
+                var res = await translator.Translate(new List<string>(words), lge);
+                var dic = new Dictionary<string, string>();
+                var outputFile = $"{Path.GetFileName(options.FilePath)}-{lge}.{Path.GetExtension(options.FilePath)}";
+                for (int i = 0; i < res.Count(); i++)
+                {
+                    dic.Add(resources.Keys.ElementAt(i), res.ElementAt(i));
+                }
+                resxHandler.Create(dic, outputFile, options.OutPutPath);
+                Console.WriteLine($"Translations finished and file created: ${outputFile}");
+            }
         }
     }
 }
