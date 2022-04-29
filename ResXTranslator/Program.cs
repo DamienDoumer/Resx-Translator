c47cc05da1acfd52ea54786b2fc42f5e32eec66f
Brian Beversdorf <bbeversdorf@gowithfloat.com>
2022-04-29 17:12:00 -0500

    Fixes Broken Multi-Language Translations When Existing Flag Is Enabled

--- ResXTranslator/Program.cs
+++ ResXTranslator/Program.cs
@@ -29,27 +29,29 @@ namespace ResxTranslator
             var resources = resxHandler.Read(options.FilePath);
             foreach (var lge in options.TranslationLanguages)
             {
+                var resourcesCopy = resources.Select((o) => o).ToList();
                 var outputFileName = $"{Path.GetFileNameWithoutExtension(options.FilePath)}.{lge}{Path.GetExtension(options.FilePath)}";
                 var outputFile = Path.Combine(options.OutPutPath, outputFileName);
                 var dic = new Dictionary<string, string>();
                 if (options.UseExistingTranslation && File.Exists(outputFile))
                 {
                     var existingValues = resxHandler.Read(outputFile);
-                    foreach (var resource in resources.ToList())
+                    foreach (var resource in resourcesCopy.ToList())
                     {
                         var key = resource.Key;
                         if (existingValues.ContainsKey(key))
                         {
                             dic.Add(key, existingValues[key]);
-                            resources.Remove(resource.Key);
+                            resourcesCopy.Remove(resource);
                         }
                     }
                 }
-
-                var translationResults = await translator.TranslateAsync(resources.Values, lge, options.ResourceLanguage).ConfigureAwait(false);
+                var keys = resourcesCopy.Select((o) => o.Key).ToList();
+                var values = resourcesCopy.Select((o) => o.Value).ToList();
+                var translationResults = await translator.TranslateAsync(values, lge, options.ResourceLanguage).ConfigureAwait(false);
                 for (int i = 0; i < translationResults.Count(); i++)
                 {
-                    var key = resources.Keys.ElementAt(i);
+                    var key = keys.ElementAt(i);
                     dic.Add(key, translationResults.ElementAt(i));
                 }
                 resxHandler.Create(dic, outputFileName, options.OutPutPath);
